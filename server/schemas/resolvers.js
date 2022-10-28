9eef3caa0066df3329bd4bb8dacd7ed83be0dab8
Codeyg12 <codey.gallup@gmail.com>
2022-10-27 18:03:36 -0600

    started schema & model

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,27 +1,40 @@
-const { xx, xx } = require('../models');
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require('../utils/auth')
 
 const resolvers = {
   Query: {
-    xx: async () => {
-      return xx.find({});
-    },
-    xx: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return xxx.find(params);
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await UserInputError.findOne({
+          _id: context.user._id,
+        });
+        return userData;
+      }
+      throw new AuthenticationError("Log in to see your projects");
     },
   },
   Mutation: {
-    createxx: async (parent, args) => {
-      const xx = await xx.create(args);
-      return xx;
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Invalid email or password");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Invalid email or password");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
     },
-    createxx: async (parent, { _id, xxNum }) => {
-      const vxx = await xx.findOneAndUpdate(
-        { _id },
-        { $inc: { [`xx${xxNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return xx;
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
   },
 };
