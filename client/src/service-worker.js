fae61078978f26968a1b00df04cd2a6ccfb3e439
John Roller <jtroller33@gmail.com>
2022-11-01 19:05:47 -0500

    service-worker done but not customized

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -1,11 +1,7 @@
 /* eslint-disable no-restricted-globals */
 
-// This service worker can be customized!
-// See https://developers.google.com/web/tools/workbox/modules
-// for the list of available Workbox modules, or add any other
-// code you'd like.
-// You can also remove this file if you'd prefer not to use a
-// service worker, and the Workbox build step will be skipped.
+//  https://developers.google.com/web/tools/workbox/modules
+
 
 import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
