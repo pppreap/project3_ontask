23e6cbe7a25db6512fd95d236985f01f47f359e6
John Roller <jtroller33@gmail.com>
2022-11-02 21:38:25 -0500

    service worker done

--- client/src/index.js
+++ client/src/index.js
@@ -3,21 +3,17 @@ import ReactDOM from 'react-dom';   // import ReactDOM from 'react-dom/client'?
 import './index.css';
 import App from './App';
 
-import { Workbox } from 'workbox-window'; // for PWA
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  // document.getElementById('root')
 );
 
-// Check if service workers are supported
-if ('serviceWorker' in navigator) {
-  // register workbox service worker
-  const workboxSW = new Workbox('/service-worker.js');
-  workboxSW.register();
-} else {
-  console.error('Service workers are not supported in this browser.');
-}
+serviceWorkerRegistration.register();
+
+reportWebVitals();
