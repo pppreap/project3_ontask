36baf90357867f16ece6d0f35079c5b43c218edd
Codeyg12 <codey.gallup@gmail.com>
2022-11-03 18:45:52 -0600

    added client side auth

--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -1,10 +1,36 @@
-import Reac, { useState } from "react";
+import { useMutation, useQuery } from "@apollo/client";
+import React, { useState } from "react";
+import { ALL_PROJECTS, ONE_PROJECTS } from "../utils/queries";
+import { ADD_PROJECT } from '../utils/mutations'
+import Auth from '../utils/auth'
 
 const ProjectPage = () => {
-    const [project, setProject] = useState({
-        title: '',
-        description: '',
-        complete: false
-    });
-    
-}
+  const [project, setProject] = useState({
+    title: "",
+    description: "",
+    complete: false,
+  });
+
+  const { allLoading, data } = useQuery(ALL_PROJECTS);
+  const projectsArray = data?.allProjects || [];
+
+  const {
+    loading: oneLoading,
+    data: oneData,
+    error: oneError,
+  } = useQuery(ONE_PROJECTS, {
+    variables: { id: projectId },
+  });
+
+  const [addProject, { addError }] = useMutation(ADD_PROJECT)
+
+  if (allLoading || oneLoading) {
+    return <h2>LOADING...</h2>
+  }
+
+  return (
+    <>
+    {!Auth.loggedIn()}
+    </>
+  )
+};
